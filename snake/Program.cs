fe37c141d42e9e4dd21413ba6d5914acb805a5e9
samusenok <samusenok@gmail.com>
2015-09-26 13:31:35 +0800

    Класс до инкапсуляции

---
+++ snake/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace snake
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Point p1 = new Point();
+            p1.x = 1;
+            p1.y = 3;
+            p1.sym = '*';
+            p1.Draw();
+            //Draw(p1.x, p1.y, p1.sym);
+
+            Point p2 = new Point();
+            p2.x = 4;
+            p2.y = 5;
+            p2.sym = '#';
+            p2.Draw();
+            
+      
+            Console.ReadLine();
+        }
+                
+    }
+}
