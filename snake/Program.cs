4209cb57134a46018104af8870d20d1e090c116c
samusenok <samusenok@gmail.com>
2015-10-03 14:48:43 +0800

    Изучение класса СПИСОК (List)

--- snake/Program.cs
+++ snake/Program.cs
@@ -15,7 +15,26 @@ namespace snake
             
             Point p2 = new Point(4, 5, '#');
             p2.Draw();
-            
+
+            List<int> NumList = new List<int>();
+            NumList.Add (5);
+            NumList.Add (6);
+            NumList.Add (7);
+
+            int x = NumList[0];
+            int y = NumList[1];
+            int z = NumList[2];
+
+            foreach (int i in NumList)
+            {
+                Console.WriteLine(i);
+            }
+
+            NumList.RemoveAt(0);
+
+            List<Point> pList = new List<Point>();
+            pList.Add(p1);
+            pList.Add(p2);
       
             Console.ReadLine();
         }
